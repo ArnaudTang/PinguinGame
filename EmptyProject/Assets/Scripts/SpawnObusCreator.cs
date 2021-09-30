90b808c6e3ac37a945073d5a9abfd2419d241aca
Arnaud <tangarnaud27@gmail.com>
2021-09-30 11:54:54 +0200

    Creation Obus and spawn

---
+++ EmptyProject/Assets/Scripts/SpawnObusCreator.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnObusCreator : MonoBehaviour
+{
+    GameObject m_Plane;
+    [SerializeField] GameObject obus;
+    float px = 50f;
+    float py = 10f;
+    float pz = 10f;
+
+    private void Awake()
+    {
+        
+            
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+    
+        m_Plane = GameObject.CreatePrimitive(PrimitiveType.Plane);
+        m_Plane.transform.localScale = new Vector3(px, py, pz);
+        m_Plane.transform.SetParent(gameObject.transform);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameObject new_Obus = Instantiate(obus);
+        float randomX = Random.Range(0,px);
+        float randomY = Random.Range(10, 30);
+        float randomZ = Random.Range(0, pz);
+        Vector3 Position = new Vector3 (randomX,randomY,randomZ);
+        new_Obus.transform.localPosition = Position;
+    }
+}
