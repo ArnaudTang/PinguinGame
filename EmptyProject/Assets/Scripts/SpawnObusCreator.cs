11b9c00bd804cd222861b4c662f8f0b7421e312b
Sho <tangarnaud27@gmail.com>
2021-11-14 04:59:43 +0100

    Implémentation d'arbre

--- EmptyProject/Assets/Scripts/SpawnObusCreator.cs
+++ EmptyProject/Assets/Scripts/SpawnObusCreator.cs
@@ -7,6 +7,7 @@ public class SpawnObusCreator : MonoBehaviour
     GameObject bombContainer;
     GameObject m_plane;
     GameObject m_player;
+    GameObject trees;
 
     ScoreSetter scoreSetter;
 
@@ -15,6 +16,7 @@ public class SpawnObusCreator : MonoBehaviour
     [SerializeField] GameObject obus;
     [SerializeField] GameObject terrain;
     [SerializeField] GameObject player;
+    [SerializeField] GameObject tree;
     [SerializeField] float px = 3000f;
     [SerializeField] float py = 1f;
     [SerializeField] float pz = 50f;
@@ -32,6 +34,7 @@ public class SpawnObusCreator : MonoBehaviour
         m_plane.transform.localScale = new Vector3(px, py, pz);
         m_plane.transform.position = new Vector3(px/2, 0, pz/2);
         m_plane.transform.SetParent(gameObject.transform);
+        trees = new GameObject("trees");
 
         StartGame();
     }
@@ -54,6 +57,26 @@ public class SpawnObusCreator : MonoBehaviour
             Vector3 Position = new Vector3(randomX, randomY, randomZ);
             new_Obus.transform.localPosition = Position;
             new_Obus.transform.SetParent(bombContainer.transform);
+           
+            
+            //float tree_pos_x = 20f;
+            float tree_pos_y = 1.5f;
+            float left_tree_pos_z = -10f;
+            float right_tree_pos_z = 60f;
+            
+            
+            for (int i = 0; i <= px; i = i + 200) 
+            {
+                GameObject new_left_tree = Instantiate(tree);
+                GameObject new_right_tree = Instantiate(tree);
+                Vector3 right_tree_position = new Vector3(i, tree_pos_y, right_tree_pos_z);
+                Vector3 left_tree_position = new Vector3(i, tree_pos_y, left_tree_pos_z);
+                new_right_tree.transform.localPosition = right_tree_position;
+                new_left_tree.transform.localPosition = left_tree_position;
+                new_left_tree.transform.SetParent(trees.transform);
+                new_right_tree.transform.SetParent(trees.transform);
+            }
+            
         }
 
         if (m_player.transform.position.z < 0) {
