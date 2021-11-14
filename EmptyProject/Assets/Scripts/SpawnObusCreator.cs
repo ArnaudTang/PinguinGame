cb777130f97d16c98162c1e070859af9a3814bcc
Clement <clem.maurer@hotmail.fr>
2021-11-14 18:59:04 +0100

    Spawn stuff and add materials

--- EmptyProject/Assets/Scripts/SpawnObusCreator.cs
+++ EmptyProject/Assets/Scripts/SpawnObusCreator.cs
@@ -7,16 +7,18 @@ public class SpawnObusCreator : MonoBehaviour
     GameObject bombContainer;
     GameObject m_plane;
     GameObject m_player;
-    GameObject trees;
+    GameObject objects;
 
     ScoreSetter scoreSetter;
 
     bool hasSpawned = false;
 
+    [SerializeField] GameObject[] toSpawn;
+    [SerializeField] int spawn_step = 3;
+
     [SerializeField] GameObject obus;
     [SerializeField] GameObject terrain;
     [SerializeField] GameObject player;
-    [SerializeField] GameObject tree;
     [SerializeField] float px = 3000f;
     [SerializeField] float py = 1f;
     [SerializeField] float pz = 50f;
@@ -31,10 +33,12 @@ public class SpawnObusCreator : MonoBehaviour
     void Start()
     {
         m_plane = Instantiate(terrain);
-        m_plane.transform.localScale = new Vector3(px, py, pz);
-        m_plane.transform.position = new Vector3(px/2, 0, pz/2);
+        terrain.transform.localScale = new Vector3(px, py, pz);
+        terrain.transform.position = new Vector3(px / 2, 0, pz / 2);
         m_plane.transform.SetParent(gameObject.transform);
-        trees = new GameObject("trees");
+
+        Material mat = Resources.Load("Grass") as Material;
+        m_plane.GetComponent<MeshRenderer>().materials[0] = mat;        
 
         StartGame();
     }
@@ -53,30 +57,10 @@ public class SpawnObusCreator : MonoBehaviour
             new_Obus.tag = "Obus";
             float randomX = Random.Range(Mathf.Max(pos_player - 10, 15), px);
             float randomY = Random.Range(80, 100);
-            float randomZ = Random.Range(2, pz - 2);
+            float randomZ = Random.Range(3, pz - 3);
             Vector3 Position = new Vector3(randomX, randomY, randomZ);
             new_Obus.transform.localPosition = Position;
-            new_Obus.transform.SetParent(bombContainer.transform);
-           
-            
-            //float tree_pos_x = 20f;
-            float tree_pos_y = 1.5f;
-            float left_tree_pos_z = -10f;
-            float right_tree_pos_z = 60f;
-            
-            
-            for (int i = 0; i <= px; i = i + 200) 
-            {
-                GameObject new_left_tree = Instantiate(tree);
-                GameObject new_right_tree = Instantiate(tree);
-                Vector3 right_tree_position = new Vector3(i, tree_pos_y, right_tree_pos_z);
-                Vector3 left_tree_position = new Vector3(i, tree_pos_y, left_tree_pos_z);
-                new_right_tree.transform.localPosition = right_tree_position;
-                new_left_tree.transform.localPosition = left_tree_position;
-                new_left_tree.transform.SetParent(trees.transform);
-                new_right_tree.transform.SetParent(trees.transform);
-            }
-            
+            new_Obus.transform.SetParent(bombContainer.transform);      
         }
 
         if (m_player.transform.position.z < 0) {
@@ -108,6 +92,7 @@ public class SpawnObusCreator : MonoBehaviour
         {
             Destroy(m_player);
             Destroy(bombContainer);
+            Destroy(objects);
         }
 
         score = 0f;
@@ -117,5 +102,20 @@ public class SpawnObusCreator : MonoBehaviour
         m_player.transform.Rotate(new Vector3(0, 90, 0));
         m_player.transform.position = new Vector3(10, 3, pz / 2);
         scoreSetter = FindObjectOfType<ScoreSetter>();
+
+        float pos_y = 0f;
+
+        objects = new GameObject("objects");
+
+        for (int i = 10; i <= px; i = i + spawn_step)
+        {
+            float randomZ = Random.Range(2, pz - 2);
+            int randomIndex = Mathf.FloorToInt(Random.Range(0, toSpawn.Length));
+            GameObject new_obj = Instantiate(toSpawn[randomIndex]);
+            Vector3 position = new Vector3(i, pos_y, randomZ);
+            new_obj.transform.localPosition = position;
+            new_obj.tag = "Terrain";
+            new_obj.transform.SetParent(objects.transform);
+        }
     }
 }
